fc87de5347108b37b64a6a36a8364350babec466
Olive <olivelliott48@gmail.com>
2022-06-14 18:14:18 -0400

    Add mock Potion and refactor player constructor to hold inventory

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -1,4 +1,8 @@
 const Player = require('../lib/Player');
+const Potion = require('../lib/Potion');
+
+jest.mock('../lib/Potion')
+
 
 test('creates a player object', () => {
     const player = new Player('Dave');
@@ -7,4 +11,5 @@ test('creates a player object', () => {
     expect(player.health).toEqual(expect.any(Number));
     expect(player.strength).toEqual(expect.any(Number));
     expect(player.agility).toEqual(expect.any(Number));
+    expect(player.inventory).toEqual(expect.arrayContaining([expect.any(Object)]));
 })
