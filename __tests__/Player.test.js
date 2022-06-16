8a4d220425dc3aa657f13c7339956ed4fb8a0493
Olive <olivelliott48@gmail.com>
2022-06-15 20:25:41 -0400

    Test and build Players health methods

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -23,12 +23,34 @@ test('gets player"s stats as an object', () => {
     expect(player.getStats()).toHaveProperty('agility');
 });
 
-
 test('gets inventory from player or returns false', () => {
     const player = new Player('Dave');
-  
     expect(player.getInventory()).toEqual(expect.any(Array));
     player.inventory = [];
-  
     expect(player.getInventory()).toEqual(false);
   });
+
+  test('gets players health value', () => {
+    const player = new Player('Dave');
+    expect(player.getHealth()).toEqual(expect.stringContaining(player.health.toString()))
+});
+
+test('checks if player is alive or not', () => {
+    const player = new Player('Dave');
+    expect(player.isAlive()).toBeTruthy();
+    player.health = 0;
+    expect(player.isAlive()).toBeFalsy();
+})
+
+test('subtracts from players health', () => {
+    const player = new Player('Dave');
+    const oldHealth = player.health;
+
+    player.reduceHealth(5);
+
+    expect(player.health).toBe(oldHealth - 5);
+
+    player.reduceHealth(99_999);
+
+    expect(player.health).toBe(0);
+})
